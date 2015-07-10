32e013c7f59e787d52906f14e382a83d3938a21b
Alex Chistyakov <alexclear@gmail.com>
2015-07-10 09:55:48 +0300

    Make path and user configurable

--- templates/hdfs/datanode.rb
+++ templates/hdfs/datanode.rb
@@ -1,12 +1,12 @@
-Eye.application 'hdfs-datanode' do
+Eye.application 'hdfs-datanode-{{ env_name }}' do
   working_dir '/etc/eye'
-  stdall '/var/log/eye/hdfs-datanode-stdall.log' # stdout,err logs for processes by default
+  stdall '/var/log/eye/hdfs-datanode-{{ env_name }}-stdall.log' # stdout,err logs for processes by default
   trigger :flapping, times: 10, within: 1.minute, retry_in: 3.minutes
   check :cpu, every: 10.seconds, below: 100, times: 3 # global check for all processes
 
-  process :datanode do
-    pid_file '/var/hadoop/hdfs_datanode.pid'
-    start_command 'sudo -u hadoop /opt/hadoop/bin/hdfs datanode'
+  process :datanode_{{ env_name }} do
+    pid_file '{{ hadoop_var_prefix }}/hdfs_datanode.pid'
+    start_command 'sudo -u {{ hadoop_user }} {{ hadoop_distr_prefix }}/bin/hdfs datanode'
 
     daemonize true
     start_timeout 10.seconds
