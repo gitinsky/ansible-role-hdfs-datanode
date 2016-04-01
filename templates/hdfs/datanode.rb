ac09f4deeb41ea62bbab274d722940b801c400e9
Alex Chistyakov <alexclear@gmail.com>
2016-04-01 16:53:43 +0300

    Use uid

--- templates/hdfs/datanode.rb
+++ templates/hdfs/datanode.rb
@@ -3,10 +3,11 @@ Eye.application 'hdfs-datanode-{{ env_name }}' do
   stdall '/var/log/eye/hdfs-datanode-{{ env_name }}-stdall.log' # stdout,err logs for processes by default
   trigger :flapping, times: 10, within: 1.minute, retry_in: 3.minutes
   check :cpu, every: 10.seconds, below: 100, times: 3 # global check for all processes
+  uid "{{ hadoop_user }}"
 
   process :datanode_{{ env_name }} do
     pid_file '{{ hadoop_var_prefix }}/hdfs_datanode.pid'
-    start_command 'sudo -u {{ hadoop_user }} {{ hadoop_distr_prefix }}/bin/hdfs datanode'
+    start_command '{{ hadoop_distr_prefix }}/bin/hdfs datanode'
 
     daemonize true
     start_timeout 10.seconds
