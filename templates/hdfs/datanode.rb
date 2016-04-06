1d6e7390e92a67ebe119091b8965ade6c1cac33d
Alex Chistyakov <alexclear@gmail.com>
2016-04-06 17:16:21 +0300

    Don't check CPU utilization in eye

--- templates/hdfs/datanode.rb
+++ templates/hdfs/datanode.rb
@@ -2,7 +2,8 @@ Eye.application 'hdfs-datanode-{{ env_name }}' do
   working_dir '/etc/eye'
   stdall '/var/log/eye/hdfs-datanode-{{ env_name }}-stdall.log' # stdout,err logs for processes by default
   trigger :flapping, times: 10, within: 1.minute, retry_in: 3.minutes
-  check :cpu, every: 10.seconds, below: 100, times: 3 # global check for all processes
+#  Don't terminate on high CPU utilization
+#  check :cpu, every: 10.seconds, below: 100, times: 3 # global check for all processes
   uid "{{ hadoop_user }}"
 
   process :datanode_{{ env_name }} do
